5632fa7cfdfda6520265c1b659efbd136c5cd7b4
hayesHowYaDoin <jordanhayes98@gmail.com>
2025-11-14 23:09:23 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,81 @@
+{
+  description = "Nix configuration for the Shevek host";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.home-manager.follows = "home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    cli-config = {
+      url = "github:hayesHowYaDoin/cli_config";
+      inputs.home-manager.follows = "home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    desktop-config = {
+      url = "github:hayesHowYaDoin/desktop_config";
+      inputs.home-manager.follows = "home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    stylix,
+    cli-config,
+    desktop-config,
+    ...
+  } @inputs:
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+    };
+
+    user = {
+      name = "jordan";
+      home = "/home/jordan";
+      fullName = "Jordan Hayes";
+      gitUser = "hayesHowYaDoin";
+      gitEmail = "jordanhayes98@gmail.com";
+    };
+  in {
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs user; };
+        modules = [
+          ./host/configuration.nix
+          stylix.nixosModules.stylix
+        ];
+      };
+    };
+
+    homeConfigurations = {
+      default = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit inputs pkgs user; };
+        modules = [
+          ./home/default.nix
+          cli-config.homeManagerModules.default
+          desktop-config.homeManagerModules.default
+          stylix.homeModules.stylix
+        ];
+      };
+    };
+  }
+}
---
+++ home/default.nix
@@ -0,0 +1,64 @@
+{
+  config,
+  lib,
+  pkgs,
+  user,
+  ...
+}: {
+  stylix = {
+    enable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    image = ../assets/wallpaper.jpeg;
+  };
+
+  features = {
+    cli = {
+      git = {
+        enable = true;
+        userName = user.gitUser;
+        userEmail = user.gitEmail;
+      };
+      nvim = {
+        enable = true;
+        theme = {
+          name = "catppuccin";
+          style = "mocha";
+        };
+      };
+      zsh.enable = true;
+    };
+    desktop = {
+      fonts.enable = true;
+      applications = {
+        ghostty = {
+          enable = true;
+          opacity = 0.9;
+          shader = ../assets/cursor_tail.glsl;
+        };
+        obsidian.enable = true;
+      };
+      environment = {
+        plasma.enable = true;
+      };
+    };
+  };
+
+  home = {
+    username = user.name;
+    homeDirectory = user.home;
+    stateVersion = "24.11";
+    packages = with pkgs; [
+      spotify
+      godot_4
+      discord
+    ];
+  };
+
+  # Stylix can't replace this file and deleting it has no obvious concequence
+  home.activation.removeBackups = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
+    rm -f ${config.home.homeDirectory}/.gtkrc-2.0
+    rm -f ${config.home.homeDirectory}/.config/user-dirs.dirs
+  '';
+
+  programs.home-manager.enable = true;
+}
